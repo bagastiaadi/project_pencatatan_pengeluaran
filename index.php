4bf9956f0fc43957c4dc3978e490438cbcae38f9
AryaWijaya777 <240030139@stikom-bali.ac.id>
2026-01-06 21:39:26 +0800

    Menambahkan kode pada index.php untuk mengecek status login user dan mengarahkan ke halaman yang sesuai

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-phpinfo();
+
+require_once 'autoload.php';
+
+// Cek apakah user sudah login
+if (isLoggedIn()) {
+    redirect('pages/dashboard.php');
+} else {
+    redirect('pages/login.php');
+}
+?>
